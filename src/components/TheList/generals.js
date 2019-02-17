72e05f5b3da852ef4e5f83f0b60ed7e78cd9c9c4
Wooj <wjchung@hotmail.ca>
2019-02-16 21:36:45 -0800

    changed up look of card a lot, added click button and ranking now on top left

--- src/components/TheList/generals.js
+++ src/components/TheList/generals.js
@@ -3,11 +3,19 @@ import React from 'react'
 const Generals = ({ name, portrait, rank, nationality }) => (
   <div className="generalList--column--3">
     <div className="card">
+      <div className="card--ribbon">
+        <div className="card--ribbon--text"> #{rank} </div>
+      </div>
       <img alt={ name } className="card--image" src={ portrait } />
       <div className="card--content">
         <h3 className="card--content--title">{ name }</h3>
-        <p><b>Rank:</b> { rank }</p>
+        {/* <p><b>Rank:</b> { rank }</p> */}
         <p><b>Nationality: </b> { nationality }</p>
+      </div>
+      <div className="card--hidden">
+        <div className="card--hidden--button">
+          More...
+        </div>
       </div>
     </div>
   </div>
